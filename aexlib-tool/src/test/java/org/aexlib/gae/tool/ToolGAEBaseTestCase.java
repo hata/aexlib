7a4db10889120148537e7e46a06850fbfaae5671
Hiroki Ata <hiroki.ata@gmail.com>
2010-02-09 23:59:54 -0800

    Added test case.

---
+++ aexlib-tool/src/test/java/org/aexlib/gae/tool/ToolGAEBaseTestCase.java
@@ -0,0 +1,56 @@
+/*
+ * $Id$
+ * 
+ * Copyright 2010 Hiroki Ata
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.aexlib.gae.tool;
+
+import java.io.File;
+
+import org.aexlib.gae.TestEnvironment;
+
+import com.google.appengine.api.datastore.dev.LocalDatastoreService;
+import com.google.appengine.tools.development.ApiProxyLocalImpl;
+import com.google.apphosting.api.ApiProxy;
+
+import junit.framework.TestCase;
+
+public class ToolGAEBaseTestCase extends TestCase {
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        ApiProxy.setEnvironmentForCurrentThread(new TestEnvironment());
+        ApiProxy.setDelegate(new ApiProxyLocalImpl(new File("target")){});
+        ApiProxyLocalImpl proxy = (ApiProxyLocalImpl) ApiProxy.getDelegate();
+        proxy.setProperty(LocalDatastoreService.NO_STORAGE_PROPERTY, Boolean.TRUE.toString());
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        ApiProxyLocalImpl proxy = (ApiProxyLocalImpl) ApiProxy.getDelegate();
+        LocalDatastoreService datastoreService = (LocalDatastoreService) proxy.getService("datastore_v3");
+        datastoreService.clearProfiles();
+        // not strictly necessary to null these out but there's no harm either
+        ApiProxy.setDelegate(null);
+        ApiProxy.setEnvironmentForCurrentThread(null);
+        super.tearDown();
+    }
+
+    public void testDummy() {
+        
+    }
+
+}
